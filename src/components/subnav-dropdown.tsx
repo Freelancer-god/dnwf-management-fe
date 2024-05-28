98fee08026aa8b6cd60d3d9901c3e2cc0ad77c28
duchuy-wins <luongduchuy11b1@gmail.com>
2024-05-28 07:57:44 +0700

    Fix: nested Subnav

--- src/components/subnav-dropdown.tsx
+++ src/components/subnav-dropdown.tsx
@@ -8,14 +8,18 @@ import {
 } from "@/components/ui/accordion";
 
 export default function SubnavDropdown({ item }: { item: MenuItem }) {
-  return item.subItems && item.subItems.length > 0 ? (
+  const hasSubNav = item.subItems && item.subItems.length > 0;
+
+  return hasSubNav ? (
     <Accordion type="single" collapsible className="w-full !border-b-0">
       <AccordionItem value="item-1">
         <AccordionTrigger className="py-2">
           <NavItem item={item} />
         </AccordionTrigger>
         <AccordionContent className="ml-2">
-          {item.subItems?.map((subitem: MenuItem) => <NavItem key={subitem.href} item={subitem} />)}
+          {item.subItems?.map((subitem: MenuItem) => (
+            <SubnavDropdown key={subitem.href} item={subitem} />
+          ))}
         </AccordionContent>
       </AccordionItem>
     </Accordion>

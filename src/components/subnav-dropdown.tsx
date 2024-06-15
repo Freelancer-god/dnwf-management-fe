3d1e8061167def9c3535eb1876f06bb079a18eb1
duchuy-wins <luongduchuy11b1@gmail.com>
2024-06-15 16:08:07 +0700

    Fix: css

--- src/components/subnav-dropdown.tsx
+++ src/components/subnav-dropdown.tsx
@@ -1,11 +1,6 @@
 "use client";
 import NavItem from "@/components/nav-item";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { MenuItem } from "@/types/menu-item";
 
 export default function SubnavDropdown({ item }: { item: MenuItem }) {
@@ -14,13 +9,11 @@ export default function SubnavDropdown({ item }: { item: MenuItem }) {
   return hasSubNav ? (
     <Accordion type="single" collapsible className="w-full !border-b-0">
       <AccordionItem value="item-1">
-        <AccordionTrigger className="py-2">
+        <AccordionTrigger className="">
           <NavItem item={item} />
         </AccordionTrigger>
         <AccordionContent className="ml-2">
-          {item.subItems?.map((subitem: MenuItem) => (
-            <SubnavDropdown key={subitem.href} item={subitem} />
-          ))}
+          {item.subItems?.map((subitem: MenuItem) => <SubnavDropdown key={subitem.href} item={subitem} />)}
         </AccordionContent>
       </AccordionItem>
     </Accordion>
